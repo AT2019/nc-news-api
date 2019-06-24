0650dc549cbf2c6d5cfc67de3caea45053bbd07f
Anke Teale <anketeale@Ankes-MacBook-Air.local>
2019-06-24 14:04:16 +0100

    created comments table in migration

---
+++ db/migrations/20190624135855_create_comments_table.js
@@ -0,0 +1,14 @@
+exports.up = function(connection, Promise) {
+  return connection.schema.createTable("comments", function(commentsTable) {
+    commentsTable.increments("comment_id").primary();
+    commentsTable.string("author").references("users.username");
+    commentsTable.integer("article_id").references("articles.article_id");
+    commentsTable.integer("votes").defaultsTo(0);
+    commentsTable.timestamp("created_at");
+    commentsTable.text("body");
+  });
+};
+
+exports.down = function(connection, Promise) {
+  return connection.schema.dropTable("comments");
+};
